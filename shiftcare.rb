da37778784339368cb3a9ea73d612dc76a3d47c5
Rasna Shakya <rasna@gurzu.com>
2025-07-30 00:01:54 +0545

    feat: Added method to detect duplicate emails

--- shiftcare.rb
+++ shiftcare.rb
@@ -5,16 +5,37 @@ def load_clients(file)
   JSON.parse(json)
 end
 
+# Method to search client by full_name
 def search_clients(clients, query)
   clients.select do |client|
     client['full_name'].to_s.downcase.include?(query.downcase)
   end
 end
 
-if ARGV[0] == 'search' && ARGV[1]
-  clients = load_clients('clients.json')
+# Method to find_duplicate emails
+def find_duplicate_emails(clients)
+  groups = clients.group_by { |c| c['email'].to_s.downcase.strip }
+  groups.select { |_, group| group.size > 1 }
+end
+
+command = ARGV[0]
+clients = load_clients('clients.json')
+
+if command == 'search' && ARGV[1]
   results = search_clients(clients, ARGV[1])
-  results.each { |c| puts "id:#{c['id']}, full_name: #{c['full_name']}, email: #{c['email']}" }
+  results.each { |c| puts "ID:#{c['id']}, Full Name: #{c['full_name']}, Email: #{c['email']}" }
+elsif command == 'duplicates'
+  duplicates = find_duplicate_emails(clients)
+  if duplicates.empty?
+    puts 'There are no duplicate emails found'
+  else
+    duplicates.each do |email, group|
+      puts "Duplicate email: #{email}"
+      group.each { |c| puts "  - ID: #{c['id']}, Full Name: #{c['full_name']}" }
+    end
+  end
 else
-  puts 'For Searching clients, run: ruby shiftcare.rb search <name>'
+  puts 'Instructions:'
+  puts '- For searching clients, run: ruby shiftcare.rb search <name>'
+  puts '- For findind duplicate clients, run: ruby shiftcare.rb duplicates'
 end
